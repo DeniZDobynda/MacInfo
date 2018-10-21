d06bdb76eb5861df479cc12ac3997111b8efde20
Denis Dobanda <denisdarm@gmail.com>
2018-10-21 11:58:42 +0200

    Initial Commit

---
+++ MacInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacInfo
+//
+//  Created by Denis Dobanda on 21.10.18.
+//  Copyright © 2018 Denis Dobanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
